b861f92922ab7f6479b9f9ff66517acbe2899089
Link4Electronics <link4electronics@gmail.com>
2026-03-31 16:57:34 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,6 +10,7 @@ export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/256x256/apps/sayonara.png
 export DESKTOP=/usr/share/applications/com.sayonara-player.Sayonara.desktop
+export STARTUPWMCLASS=com.sayonara-player.Sayonara
 export DEPLOY_QT=1
 export QT_DIR=qt5
 
