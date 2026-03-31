e1c3d503b7209df2de8b78fdf14d69151ef7b418
Link4Electronics <link4electronics@gmail.com>
2026-03-31 16:20:45 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,21 +6,42 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm   \
+    cmake                 \
+    gst-libav             \
+    gst-plugins-bad       \
+    gst-plugins-base      \
+    gst-plugins-base-libs \
+    gst-plugins-good      \
+    gst-plugins-ugly      \
+    gstreamer             \
+    hicolor-icon-theme    \
+    python-dbus           \
+    qt5-base              \
+    qt5-svg               \
+    qt5-tools             \
+    qt5-wayland           \
+    taglib
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+echo "Building Sayonara Player..."
+echo "---------------------------------------------------------------"
+REPO="https://gitlab.com/luciocarreras/sayonara-player"
+if [ "${DEVEL_RELEASE-}" = 1 ]; then
+    echo "Making nightly build of Sayonara Player..."
+    echo "---------------------------------------------------------------"
+    VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+    git clone --depth 1 "$REPO" ./sayonara
+else
+	echo "Making stable build of Sayonara Player..."
+	VERSION="$(git ls-remote --tags "$REPO" | grep -oP '[0-9]+\.[0-9]+\.[0-9]+-stable[0-9]+$' | sort -V | tail -n1)"
+    git clone --branch "$VERSION" --single-branch --depth 1 "$REPO" ./sayonara
+fi
+echo "$VERSION" > ~/version
 
-# If the application needs to be manually built that has to be done down here
-
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+cmake -B build -S ./sayonara -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr -DWITH_TESTS=0
+cmake --build build -j$(nproc)
+cmake --install build
